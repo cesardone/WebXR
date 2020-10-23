d66a67343f14f508e7457454a5c308dffb878111
Cesar Done <cesardone@live.com>
2020-10-23 16:24:43 -0400

    Update app.js

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -16,15 +16,16 @@ class App {
     this.camera.position.set(0, 0, 4);
 
     // Scene
-    this.scene = new THREE.scene();
+    this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xaaaaaa);
 
+    // Ambient lighting
     const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
     this.scene.add(ambient);
-
     const light = new THREE.DirectionalLight();
     light.position.set(0.2, 1, 1);
     this.scene.add(light);
+
     // Renderer
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -32,23 +33,29 @@ class App {
     container.appendChild(this.renderer.domElement);
 
     // Called 60fps if browser can handle
-    this.renderer.setAnimationLoop(this.renderer.bind(this));
+    this.renderer.setAnimationLoop(this.render.bind(this));
 
     // Create a Box
     const geometry = new THREE.BoxBufferGeometry();
     const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-
     this.mesh = new THREE.Mesh(geometry, material);
+    // Add Box to the scene
     this.scene.add(this.mesh);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     window.addEventListener('resize', this.resize.bind(this));
   }
 
-  resize() {}
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
   render() {
     this.mesh.rotateY(0.01);
-    this.renderer.render(this.camera);
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
